0c010f9c7f3e9fc7ccf20b12ad2c5b4b712f4ef6
pirainogi <pirainogi@gmail.com>
2019-06-24 19:58:28 -0400

    fixed copypasta issues

--- scripts/app.js
+++ scripts/app.js
@@ -3,23 +3,36 @@ window.addEventListener('DOMContentLoaded', (e) => {
 
   const imageContainter = document.querySelector(".hero-image")
 
+  carousel = [
+    "./images/hero-image-0.jpg",
+    "./images/hero-image-1.jpg",
+    "./images/hero-image-2.jpg",
+    "./images/hero-image-3.jpg",
+  ]
+
 
   function changeImages() {
     setTimeout(function (){
+      console.log("1 second");
       let currentImage = document.querySelector(".image")
-      console.log("changed an image", currentImage.style);
+
+      // for (let j = 0; j<4; j++){
+      //   currentImage.style.backgroundImage = `url("${carousel[j]}")`
+      //   if(j === 3) j = 0
+      // }
       if (currentImage.style.backgroundImage === `url("./images/hero-image-0.jpg")`){
         currentImage.style.backgroundImage = `url("./images/hero-image-1.jpg")`
       }
-      else if (currentImage.style.backgroundImage ===`url("./images/hero-image-1.jpg")`){
-        currentImage.style.backgroundImage = `"url("./images/hero-image-2.jpg")`
+      else if (currentImage.style.backgroundImage === `url("./images/hero-image-1.jpg")`){
+        currentImage.style.backgroundImage = `url("./images/hero-image-2.jpg")`
       }
-      else if (currentImage.style.backgroundImage === `url("./images/hero-image-2.jpg")`){
+      else if (currentImage.style.backgroundImage ===  `url("./images/hero-image-2.jpg")`){
         currentImage.style.backgroundImage = `url("./images/hero-image-3.jpg")`
       }
-      else if (currentImage.style.backgroundImage === `url("./images/hero-image-3.jpg")`){
+      else if (currentImage.style.backgroundImage ===  `url("./images/hero-image-3.jpg")`){
         currentImage.style.backgroundImage = `url("./images/hero-image-0.jpg")`
       }
+      // let currentImage = document.querySelector(".image")
       changeImages();
     }, 1000)
   }
