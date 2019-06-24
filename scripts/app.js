5598596e1d7fc6f67f9122db7afb604253ef3758
pirainogi <pirainogi@gmail.com>
2019-06-24 19:29:36 -0400

    it works but the code aint pretty

---
+++ scripts/app.js
@@ -0,0 +1,29 @@
+window.addEventListener('DOMContentLoaded', (e) => {
+    console.log('DOM fully loaded and parsed');
+
+  const imageContainter = document.querySelector(".hero-image")
+
+
+  function changeImages() {
+    setTimeout(function (){
+      let currentImage = document.querySelector(".image")
+      console.log("changed an image", currentImage.style);
+      if (currentImage.style.backgroundImage === `url("./images/hero-image-0.jpg")`){
+        currentImage.style.backgroundImage = `url("./images/hero-image-1.jpg")`
+      }
+      else if (currentImage.style.backgroundImage ===`url("./images/hero-image-1.jpg")`){
+        currentImage.style.backgroundImage = `"url("./images/hero-image-2.jpg")`
+      }
+      else if (currentImage.style.backgroundImage === `url("./images/hero-image-2.jpg")`){
+        currentImage.style.backgroundImage = `url("./images/hero-image-3.jpg")`
+      }
+      else if (currentImage.style.backgroundImage === `url("./images/hero-image-3.jpg")`){
+        currentImage.style.backgroundImage = `url("./images/hero-image-0.jpg")`
+      }
+      changeImages();
+    }, 1000)
+  }
+
+  changeImages()
+
+}) // end of dom listener
